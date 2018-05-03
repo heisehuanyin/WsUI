4f8678ed320f37c7be34066c0f2e3b51c21d7cc9
heisehuanyin <2422523675@qq.com>
2018-05-03 15:45:10 +0800

    nothing

--- src/ws/mechanism/binding/ObserveredAutoAction.java
+++ src/ws/mechanism/binding/ObserveredAutoAction.java
@@ -1,7 +1,7 @@
 package ws.mechanism.binding;
 
-public abstract interface ObserveredAutoAction {
-	abstract void _bindingFrom(ContainerBinding c);
-	abstract void _sync_to(ObserveredAutoAction front);
-	abstract void actionPerferm(String typeMsg);
+public interface ObserveredAutoAction {
+	void _bindingFrom(ContainerBinding c);
+	void _sync_to(ObserveredAutoAction front);
+	void actionPerferm(String typeMsg);
 }
